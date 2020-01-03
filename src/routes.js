34a02169f5c17c91260ed218ef7b821aa25eb6d8
felipe <fajreyy.bh@gmail.com>
2020-01-03 19:08:36 -0300

    feat: adding recipes routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
 
+import RecipesController from './app/controllers/RecipesController'
 import FileController from './app/controllers/FileController'
 
 const routes = new Router()
 const upload = multer(multerConfig)
 // Routes goes here
+
+routes.get('/', RecipesController.store)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
