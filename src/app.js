4a638fb52ea7b01b1abc246a7dc17818bb51ab6f
Felipe Alves de Jesus <fajreyy.bh@gmail.com>
2020-01-06 00:17:12 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+import 'dotenv/config'
 import express from 'express'
+import path from 'path'
 import routes from './routes'
+
+import * as Sentry from '@sentry/node'
+import sentryConfig from './config/sentry'
+
+import 'express-async-errors'
 
 import './database/index'
 
@@ -7,16 +14,41 @@ class AppController {
   constructor() {
     this.server = express()
 
+    Sentry.init(sentryConfig)
+
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.middlewares() 
     this.routes()
   }
 
   middlewares() {
     this.server.use(express.json())
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    )
+    this.server.use(
+      '/avatars',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    )
   }
 
   routes() {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if(process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON()
+
+        return res.status(500).json(errors)
+      }
+
+      return res.status(500).json({ error: 'Internal server error'})
+    })
   }
 }
 
