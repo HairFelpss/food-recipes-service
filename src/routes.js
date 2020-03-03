011ddca3af389c303c3b39f009350c0e93684b74
felipe <fajreyy.bh@gmail.com>
2020-03-03 12:15:51 -0300

    gets without authorization

--- src/routes.js
+++ src/routes.js
@@ -1,47 +1,48 @@
-import { Router } from 'express'
-import multer from 'multer'
-import multerConfig from './config/multer'
+import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
-import UserController from './app/controllers/UserController'
-import RecipeController from './app/controllers/RecipeController'
-import FileController from './app/controllers/FileController'
-import TypeController from './app/controllers/TypeController'
-import RoleController from './app/controllers/RoleController'
-import SessionController from './app/controllers/SessionController'
+import UserController from "./app/controllers/UserController";
+import RecipeController from "./app/controllers/RecipeController";
+import FileController from "./app/controllers/FileController";
+import TypeController from "./app/controllers/TypeController";
+import RoleController from "./app/controllers/RoleController";
+import SessionController from "./app/controllers/SessionController";
 
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from "./app/middlewares/auth";
 
-const routes = new Router()
-const upload = multer(multerConfig)
+const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+routes.get("/recipes", RecipeController.index);
+routes.get("/recipes/:id", RecipeController.indexJustOne);
+routes.get("/types", TypeController.index);
 
-routes.use(authMiddleware)
+routes.use(authMiddleware);
 
-routes.get('/users', UserController.index)
-routes.put('/users/', UserController.update)
-routes.delete('/users/:id', UserController.delete)
+routes.get("/users", UserController.index);
+routes.put("/users/", UserController.update);
+routes.delete("/users/:id", UserController.delete);
 
-routes.post('/roles', RoleController.store)
-routes.get('/roles', RoleController.index)
-routes.put('/roles/:id', RoleController.update)
-routes.delete('/roles/:id', RoleController.delete)
+routes.post("/roles", RoleController.store);
+routes.get("/roles", RoleController.index);
+routes.put("/roles/:id", RoleController.update);
+routes.delete("/roles/:id", RoleController.delete);
 
-routes.post('/recipes', RecipeController.store)
-routes.get('/recipes', RecipeController.index)
-routes.get('/recipes/:id', RecipeController.indexJustOne)
-routes.put('/recipes/:id', RecipeController.update)
-routes.delete('/recipes/:id', RecipeController.delete)
+routes.post("/recipes", RecipeController.store);
 
-routes.post('/types', TypeController.store)
-routes.get('/types', TypeController.index)
-routes.put('/types/:id', TypeController.update)
-routes.delete('/types/:id', TypeController.delete)
+routes.put("/recipes/:id", RecipeController.update);
+routes.delete("/recipes/:id", RecipeController.delete);
 
-routes.post('/files', upload.single('file'), FileController.store)
-routes.get('/files', FileController.index)
-routes.put('/files/:id', upload.single('file'), FileController.update)
-routes.delete('/files/:id', FileController.delete)
+routes.post("/types", TypeController.store);
+routes.put("/types/:id", TypeController.update);
+routes.delete("/types/:id", TypeController.delete);
 
-export default routes
+routes.post("/files", upload.single("file"), FileController.store);
+routes.get("/files", FileController.index);
+routes.put("/files/:id", upload.single("file"), FileController.update);
+routes.delete("/files/:id", FileController.delete);
+
+export default routes;
